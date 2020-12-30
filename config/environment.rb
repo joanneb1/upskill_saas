f54fbd6e48aca8923d100627a8223c3034bc9fde
Joanne Borthwick <joanne.borthwick@live.co.uk>
2020-12-30 15:03:02 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'https://blooming-tor-14353.herokuapp.com',
+  :domain         => 'blooming-tor-14353.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
